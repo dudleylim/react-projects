38f6eb7c90d2ca29686537d8137ccc6c8bd25338
dudleylim <dudleylim@gmail.com>
2022-07-06 14:32:31 +0800

    updated readme, proj2

--- proj2/src/App.js
+++ proj2/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import Loading from './components/Loading';
+import Tours from './components/Tours';
+const url = 'https://course-api.netlify.app/api/react-tours-project';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  if (loading) {
+    return (
+      <main>
+        <section className="main-container">
+          <Loading />
+        </section>
+      </main>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <main>
+
+    </main>    
+    </>
   );
 }
 
---
+++ proj2/src/components/Loading.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from "react";
+
+const MultipleReturns = () => {
+    return (
+        <h1>Loading...</h1>
+    );
+};
+
+export default MultipleReturns;
